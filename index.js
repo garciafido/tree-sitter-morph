87fc12ba80408c54df2e0a32d102ac49b4eca226
andi <cubanitosgusti@msn.com>
2020-03-31 20:23:10 -0300

    refactor parseSemantic

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ try {
   module.exports.nodeTypeInfo = require("./src/node-types.json");
 } catch (_) {}
 
-module.exports.parseSemantic = require("./src/parse-semantic").parseSemantic;
+module.exports.parseSemantic = require("./src/parse-semantic").buildParseSemantic(require('tree-sitter'), require('.'));
--- src/parse-semantic.ts
+++ src/parse-semantic.ts
@@ -1,72 +1,8 @@
-import * as Parser from 'tree-sitter';
-import * as Morph from '.';
+// const Parser = require("tree-sitter");
+// import * as Morph from '.';
 
 
-class ParseSemantic {
-    parser = new Parser();
-    code: string;
 
-    constructor() {
-        this.parser.setLanguage(Morph);
-    }
-
-    private getContent(node) {
-        return this.code.substring(node.startIndex, node.endIndex);
-    }
-
-    private traverse(node) {
-        const type = node.type;
-        const NewNode = {
-            type: type,
-            value: undefined,
-            children: undefined
-        };
-
-        const children = node.namedChildren;
-
-        if (children.length === 0) {
-            NewNode.value = this.getContent(node);
-        }
-
-        const NewChildren = {};
-        for(const child of children) {
-            let childType = child.type;
-            const isList = childType.lastIndexOf('__list') > 0;
-            if (isList) {
-                childType = childType.substring(0, childType.lastIndexOf('__list'));
-            }
-            const index = childType.lastIndexOf('_');
-            if (index > 0 && node.type === childType.substring(0, index)) {
-                const item = childType.substring(index + 1);
-                if (isList) {
-                    if (!(item in NewChildren)) {
-                        NewChildren[item] = [];
-                    }
-                    NewChildren[item].push(this.traverse(child));
-                } else {
-                    if (item in NewChildren) {
-                        throw new Error(`${child.type} is not declared as list and there are many items`);
-                    }
-                    NewChildren[item] = this.traverse(child);
-                }
-            }
-        }
-
-        if (Object.keys(NewChildren).length > 0) {
-            NewNode['children'] = NewChildren;
-        } else if (Object.keys(children).length === 1) {
-            return this.traverse(children[0])
-        }
-        return NewNode
-    }
-
-    parse(sourceCode) {
-        this.code = sourceCode;
-        const tree = this.parser.parse(sourceCode);
-        return this.traverse(tree.rootNode);
-    }
-
-}
 
 // const sourceCode = `
 // abstract node NicePerson extends Person {
@@ -89,7 +25,75 @@ class ParseSemantic {
 
 // console.log(JSON.stringify(tree, null, 4));
 
-export function parseSemantic(sourceCode: string) {
-    const pt = new ParseSemantic();
-    return pt.parse(sourceCode);
+export function buildParseSemantic(Parser, Morph) {
+    function parseSemantic(sourceCode: string) {
+        class ParseSemantic {
+            parser = new Parser();
+            code: string;
+
+            constructor() {
+                this.parser.setLanguage(Morph);
+            }
+
+            private getContent(node) {
+                return this.code.substring(node.startIndex, node.endIndex);
+            }
+
+            private traverse(node) {
+                const type = node.type;
+                const NewNode = {
+                    type: type,
+                    value: undefined,
+                    children: undefined
+                };
+
+                const children = node.namedChildren;
+
+                if (children.length === 0) {
+                    NewNode.value = this.getContent(node);
+                }
+
+                const NewChildren = {};
+                for (const child of children) {
+                    let childType = child.type;
+                    const isList = childType.lastIndexOf('__list') > 0;
+                    if (isList) {
+                        childType = childType.substring(0, childType.lastIndexOf('__list'));
+                    }
+                    const index = childType.lastIndexOf('_');
+                    if (index > 0 && node.type === childType.substring(0, index)) {
+                        const item = childType.substring(index + 1);
+                        if (isList) {
+                            if (!(item in NewChildren)) {
+                                NewChildren[item] = [];
+                            }
+                            NewChildren[item].push(this.traverse(child));
+                        } else {
+                            if (item in NewChildren) {
+                                throw new Error(`${child.type} is not declared as list and there are many items`);
+                            }
+                            NewChildren[item] = this.traverse(child);
+                        }
+                    }
+                }
+
+                if (Object.keys(NewChildren).length > 0) {
+                    NewNode['children'] = NewChildren;
+                } else if (Object.keys(children).length === 1) {
+                    return this.traverse(children[0])
+                }
+                return NewNode
+            }
+
+            parse(sourceCode) {
+                this.code = sourceCode;
+                const tree = this.parser.parse(sourceCode);
+                return this.traverse(tree.rootNode);
+            }
+
+        }
+
+        const pt = new ParseSemantic();
+        return pt.parse(sourceCode);
+    }
 }
