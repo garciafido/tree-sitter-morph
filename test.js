301fbb37c8d9765d2955c90356f400589affa3b2
andi <cubanitosgusti@msn.com>
2020-04-03 16:33:05 -0300

    minor name change

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { parseSemantic } = require("./index");
 
-const tree = parseSemantic('node Person {}');
+const tree = parseSemantic('node Person { foo -> string bar -> string }');
 
 console.log(JSON.stringify(tree, null, 4));
