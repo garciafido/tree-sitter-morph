869e226f57e79cae71c66bd86dc709176d3f5e67
andi <cubanitosgusti@msn.com>
2020-04-03 18:51:19 -0300

    ugly fix

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ const { parseSemantic } = require("./index");
 
 const tree = parseSemantic('@foo node Person { foo -> string bar -> string }');
 
-console.log(JSON.stringify(tree, null, 2));
+// console.log(JSON.stringify(tree, null, 2));
